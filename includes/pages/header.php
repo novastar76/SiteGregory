a1e069057b6e71a7f197d9ca30a8d2c12435b3fc
Grégory Leg <leguaygregory@outlook.fr>
2019-12-13 14:31:54 +0100

    Site

---
+++ includes/pages/header.php
@@ -0,0 +1,13 @@
+<header>
+  <h1>Best Site Pour Noel ♥</h1>
+  <nav>
+    <ul>
+      <li><a href="index.php?page=accueil" title="Retour à la page d'accueil">Accueil</a></li>
+      <li><a href="index.php?page=contact" title="Nous contacter">Contact</a></li>
+      <li><a href="index.php?page=galerie" title="Photos">Quelques photos</a></li>
+      <li><a href="index.php?page=inscription" title="Vous inscrir">inscription</a></li>
+
+      <li><a href="index.php?page=mentions" title="Les mentions légales du site">Mentions légales</a></li>
+    </ul>
+  </nav>
+</header>
