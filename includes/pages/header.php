6869732ce470466918346239d53624d91b42ad01
Grégory Leg <leguaygregory@outlook.fr>
2019-12-13 16:06:36 +0100

    SiteWeb

--- includes/pages/header.php
+++ includes/pages/header.php
@@ -1,13 +1,24 @@
-<header>
-  <h1>Best Site Pour Noel ♥</h1>
-  <nav>
-    <ul>
-      <li><a href="index.php?page=accueil" title="Retour à la page d'accueil">Accueil</a></li>
-      <li><a href="index.php?page=contact" title="Nous contacter">Contact</a></li>
-      <li><a href="index.php?page=galerie" title="Photos">Quelques photos</a></li>
-      <li><a href="index.php?page=inscription" title="Vous inscrir">inscription</a></li>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>GregoSite</title>
+  </head>
 
-      <li><a href="index.php?page=mentions" title="Les mentions légales du site">Mentions légales</a></li>
-    </ul>
-  </nav>
-</header>
+  <body>
+    <div id="wrap">
+      <nav>
+          <ul>
+            <li><a href="index.php?page=accueil" title="Retour à la page d'accueil"><p>Accueil</p></a></li>
+            <li><a href="index.php?page=contact" title="Nous contacter"><p>Contact</p></a></li>
+            <li><a href="index.php?page=galerie" title="Photos"><p>Galerie</p></a></li>
+            <li><a href="index.php?page=inscription" title="Vous inscrir"><p>inscription</p></a></li>
+            <li><a href="index.php?page=cv" title="mon cv"><p>cv</p></a></li>
+            <li><a href="index.php?page=mentions" title="mentions"><p>mentions légale</p></a></li>
+
+          </ul>
+      </nav>
+   </div>
+</body>
+</html>
