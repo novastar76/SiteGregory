2426321bfc2b3c5630a6ff2e4244fd9a93aea358
Grégory Leg <leguaygregory@outlook.fr>
2019-12-16 00:49:21 +0100

    Noel FUN

--- includes/pages/accueil.inc.php
+++ includes/pages/accueil.inc.php
@@ -1,1 +1,1 @@
-<iframe width="560" height="315" src="https://www.youtube.com/embed/bpEmjxobvbY?start=89" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
--- includes/pages/header.php
+++ includes/pages/header.php
@@ -31,6 +31,22 @@
 
 
 <body>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/bpEmjxobvbY?start=89" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        <div id="body1">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit <br>
+             sed do eiusmod tempor incididunt ut labore et dolore magna <br>
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation <br>
+              oullamco laboris nisi ut aliquip ex ea commodo consequat.
+               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div id="body2">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit <br>
+             sed do eiusmod tempor incididunt ut labore et dolore magna <br>
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation <br>
+              oullamco laboris nisi ut aliquip ex ea commodo consequat.
+               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
 
 </body>
 
