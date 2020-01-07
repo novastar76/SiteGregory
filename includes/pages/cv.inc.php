e83c5d9cb18c31d218b206de992eed2fa9edfe30
Grégory Leg <leguaygregory@outlook.fr>
2020-01-07 16:59:02 +0100

    ça avance ! (je recule en vrais)

--- includes/pages/cv.inc.php
+++ includes/pages/cv.inc.php
@@ -6,11 +6,14 @@
     <title></title>
   </head>
   <body>
-    <div class="cv1"><h1>Grégory Leguay</h1><h2><hr><h3>À PROPOS DE MOI</h3><br><p>Je m'occupe d'aider dans le référencement, le Design ou encore la création de site internet (WordPress). De nature souriant et motivant, j'aime aider et mettre mes compétences au service des autres. Grand passionné du domaine automobile et de l'E-Sport, J'aime apprendre par moi-même grace à mon côté autodidacte. Enrichis de plusieurs métiers différent, je sais m'adapter au diverse situations. Au plaisir d'une future collaboration proche.</h2></p>
-    <div id="rond">
-    <div id="verticale">
+    <div class="cv1"><h1>Grégory Leguay</h1><h2><hr><h3>À PROPOS DE MOI</h3><br><p>Je m'occupe d'aider dans le référencement, le Design ou encore la création de site internet (WordPress). De nature souriant et motivant, j'aime aider et mettre mes compétences au service des autres. Grand passionné du domaine automobile et de l'E-Sport, J'aime apprendre par moi-même grace à mon côté autodidacte. Enrichis de plusieurs métiers différent, je sais m'adapter au diverse situations. Au plaisir d'une future collaboration proche.<br><h3><br><br><br>EXPERIENCE PRO<br></h3><br><h5>2012 - 2019 auxiliaires de vie<br><br>2019 - 2020 Référent digital</h5></h2></p>
+    <div id="rond"></div>
+    <div id="verticale"></div>
+    <div class="cv2"><h1>E X P E R T I S E</h1><p><br>WebDesign AdobePS<br>HTML/CSS WebMarketing<br>WordPress<br><br>Téléphone: 06.43.06.59.82<h2><br><br>R E F E R E N C E S<br></h2><br>Professeur,<br> Frédéric NOEL - Codeur Dev<br><h4><br><br>I N T É R Ê T</h4><br>Internet, Voyage, Moto, Japon</p>
 
-      </div></div></div>
+
+
+      </div></div>
     </div>
   </body>
 </html>
