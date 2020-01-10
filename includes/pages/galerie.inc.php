98fecc8f3a06a1c40f54d1b1b241057fffbe6680
Grégory Leg <leguaygregory@outlook.fr>
2020-01-10 09:56:24 +0100

    ça avance ! (je sais plus ou j'en suis)

--- includes/pages/galerie.inc.php
+++ includes/pages/galerie.inc.php
@@ -20,14 +20,28 @@
             <a href="./img/oui5.jpg">
             <img src="./img/oui5.jpg" alt="" />
             </a>
-            <a href="./img/image1.jpg">
-            <img src="./img/image1.jpg" alt="" />
+            <a href="./img/oui6.png">
+            <img src="./img/oui6.png" alt="" />
             </a>
-            <a href="./img/image1.jpg">
-            <img src="./img/image1.jpg" alt="" />
+            <a href="./img/oui7.jpg">
+            <img src="./img/oui7.jpg" alt="" />
             </a>
-            <a href="./img/image1.jpg">
-            <img src="./img/image1.jpg" alt="" />
+            <a href="./img/oui8.jpg">
+            <img src="./img/oui8.jpg" alt="" />
+            </a>
+       </li>
+       <li>
+            <a href="./img/oui9.jpg">
+            <img src="./img/oui9.jpg" alt="" />
+            </a>
+            <a href="./img/oui10.jpg">
+            <img src="./img/oui10.jpg" alt="" />
+            </a>
+            <a href="./img/oui11.jpg">
+            <img src="./img/oui11.jpg" alt="" />
+            </a>
+            <a href="./img/oui12.jpg">
+            <img src="./img/oui12.jpg" alt="" />
             </a>
        </li>
      </ul>
